81aa96fa45bad36e8a09ccf1fda37ef93314e163
durueda <durueda@gmail.com>
2025-04-11 12:29:57 +0300

    CommandHandler
    Definitions

--- src/CommandHandler.java
+++ src/CommandHandler.java
@@ -1,2 +1,13 @@
+import java.io.*;
+import java.util.*;
+
 public class CommandHandler {
+        private FSM fsm;
+        private Logger logger;
+
+        public CommandHandler(FSM fsm, Logger logger) {
+            this.fsm = fsm;
+            this.logger = logger;
+        }
 }
+
