9be0e990ec2edfedd2a54c0e29056dcbf1bc66a0
silanatci <silanatci11@gmail.com>
2025-04-13 21:37:33 +0300

    FSM variables

--- src/FSM.java
+++ src/FSM.java
@@ -1,3 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class FSM {
+    private Set<Character> symbols;
+    private Set<String> states;
+    private String initialState;
+    private Set<String> finalStates;
+    private Map<String, Map<Character, String>> transitions;
+
+
+    public FSM() {
+        this.symbols = new HashSet<>();
+        this.states = new HashSet<>();
+        this.finalStates = new HashSet<>();
+        this.transitions = new HashMap<>();
+        this.initialState = null;
+    }
 
 }
+
