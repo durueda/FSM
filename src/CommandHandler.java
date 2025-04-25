f294c18cc7ecadc36ca48b51aa506714348d29e5
durueda <durueda@gmail.com>
2025-04-25 21:59:53 +0300

    Added comments to the command handler

--- src/CommandHandler.java
+++ src/CommandHandler.java
@@ -1,5 +1,3 @@
-import java.io.*;
-import java.util.*;
 
 public class CommandHandler {
         private FSM fsm;
@@ -11,17 +9,22 @@ public class CommandHandler {
         }
     public String processCommand(String commandLine) {
         int commentIndex = commandLine.indexOf(';');
+        //We check if there is a ';' in the command line coming from the user.
         if (commentIndex != -1) {
             commandLine = commandLine.substring(0, commentIndex + 1);
-        }
+        }/* If ';' is found, we only take the part up to that point.
+         In this way, the text after the comment (; post) is canceled.*/
+
         String[] parts = commandLine.trim().split("\\s+", 2);
-        String command = parts[0].toUpperCase();
+        //command line is divided into 2 parts command,arguments(symbols and 1,2,3)
         String args = parts.length > 1 ? parts[1].trim() : "";
+        //If something is written after the command, we take it as an argument.
+        String command = parts[0].toUpperCase();//Since all commands are case-insensitive
 
         if (args.endsWith(";")) {
             args = args.substring(0, args.length() - 1).trim();
-        }
-        return command;
-    }
+        }//We end the command line with ; but we need to remove ; part when we move on to the process.
+
+    }}
 
 
